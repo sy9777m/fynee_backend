4a5354174a2eb4b157d1e0fe74d8e549240196df
Siyun Min <sy9777m@naver.com>
2020-11-30 09:42:56 +0900

    stop project

--- krx/models.py
+++ krx/models.py
@@ -13,9 +13,24 @@ class KRXListing(models.Model):
     representative = models.CharField(max_length=64)
     homepage = models.CharField(max_length=512)
     region = models.CharField(max_length=16)
+    data_source = models.CharField(max_length=32)
 
     class Meta:
         db_table = 'KRX Listing'
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class KRXDelisting(models.Model):
+    symbol = models.CharField(max_length=16)
+    name = models.CharField(max_length=64)
+    delisting_date = models.DateField()
+    reason = models.CharField(max_length=512)
+
+    class Meta:
+        db_table = 'KRX Delisting Data'
+        verbose_name = 'KRX Delisting'
 
     def __str__(self) -> str:
         return self.name
